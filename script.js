a9425fb198f152e52cf4c89944f2a6c85369fc68
Yurii Domaranskyi <domaranskyiyurii@gmail.com>
2022-03-07 12:48:35 +0200

    finish the task

---
+++ script.js
@@ -0,0 +1,7 @@
+
+let age=prompt("How old are you now?");
+let yearsRemaining=90-age;
+let days = yearsRemaining*365;
+let weeks=yearsRemaining*52;
+let months=yearsRemaining*12;
+alert("You have " + days + " days, " + weeks + " weeks, and " + months + " months left.")
